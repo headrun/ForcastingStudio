670375fff416ef8564cce2710dca9c69119efdff
Vinay Kumar <vinay@headrun.com>
2019-03-22 18:11:53 +0530

    Login page validations and redirection

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-  
+  $('#userLogin').on('click', function(){
+    if ($('#username').val() == '' || $('#password').val() == '') {
+      $('.alert-danger').html('<strong>Error!</strong> <span>Please fill the required fields.</span>');
+      $('.alert-danger').addClass('in');
+      $('.alert-danger').removeClass('hide');
+      setTimeout(function(){
+        $('.alert-danger').removeClass('in');
+        $('.alert-danger').addClass('hide');
+      }, 3000);
+    } else if ($('#username').val() != 'studio' || $('#password').val() != '123456789') {
+      $('.alert-danger').html('<strong>Error!</strong> <span>Please verify your details.</span>');
+      $('.alert-danger').addClass('in');
+      $('.alert-danger').removeClass('hide');
+      $('#username').val('');
+      $('#password').val('');
+      setTimeout(function(){
+        $('.alert-danger').removeClass('in');
+        $('.alert-danger').addClass('hide');
+      }, 3000);
+    } else if ($('#username').val() == 'studio' || $('#password').val() == '123456789') {
+      // var porjectsUrl = window.location.href+'projects.html';
+      window.document.location.href = 'projects.html';
+      // return true;
+    }
+  })
 })
